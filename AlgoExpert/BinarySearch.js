8996ece4265c33f4fc5147ac63f2ffeff33c9e73
Claire Moore <mooreclaire95@gmail.com>
2021-02-02 10:38:18 -0500

    Binary Search algos variations

--- AlgoExpert/BinarySearch.js
+++ AlgoExpert/BinarySearch.js
@@ -15,3 +15,39 @@ function bruteBinarySearch(array, target) {
       
       return -1;
   }
+
+  function betterBinarySearch(array, target) {
+    return searcher(array, 0, array.length - 1, target);
+  }
+  
+  function searcher(array, left, right, target) {
+      if (left > right) return -1;
+      let middle = Math.floor((right + left) / 2);
+      let potentialMatch = array[middle];
+      if (target === potentialMatch) {
+          return middle;
+      } else if (target < potentialMatch) {
+          return searcher(array, left, middle - 1, target)
+      } else {
+          return searcher(array, middle + 1, right, target)
+      }
+  }
+
+  function binarySearchWithWhile(array, target) {
+    let left = 0;
+      let right = array.length - 1;
+      
+      while (left <= right) {
+          const middle = Math.floor((left + right) / 2);
+          const potentialMatch = array[middle];
+          if (array[middle] === target) {
+              return middle
+          } else if (target < potentialMatch) {
+              right = middle - 1;
+          } else {
+              left = middle + 1
+          }
+      }
+      
+      return -1;
+  }
