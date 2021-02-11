479aae5715e43bb8c63a72aa4bd4ea1b3dc5bf8e
Claire Moore <mooreclaire95@gmail.com>
2021-02-11 18:40:59 -0500

    Added alternate code for Class Photos algo

--- AlgoExpert/ClassPhotos.js
+++ AlgoExpert/ClassPhotos.js
@@ -21,3 +21,21 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
       
     return true;
   }
+
+  function classPhotosTwo(redShirtHeights, blueShirtHeights) {
+    redShirtHeights.sort((a, b) => a -b);
+      blueShirtHeights.sort((a, b) => a - b);
+      
+      let frontRow = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
+      
+      for (let i = 0; i < redShirtHeights.length; i++) {
+          const currentRed = redShirtHeights[i];
+          const currentBlue = blueShirtHeights[i];
+          
+          if (frontRow === 'RED') {
+              if (currentRed >= currentBlue) return false;
+          } else if (currentBlue >= currentRed) return false;
+      }
+      
+    return true;
+  }
