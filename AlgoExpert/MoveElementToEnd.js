e848737ee72bfd09f0c94ee13d9e4078737a7f2e
Claire Moore <mooreclaire95@gmail.com>
2021-02-22 15:36:31 -0500

    Added optimized move element solution

--- AlgoExpert/MoveElementToEnd.js
+++ AlgoExpert/MoveElementToEnd.js
@@ -14,3 +14,21 @@ function moveElementToEnd(array, toMove) {
 	
   return array;
 }
+
+function optimizedMoveElementToEnd(array, toMove) {
+    let i = 0;
+      let j = array.length - 1;
+      while (i < j) {
+          while (i < j && array[j] === toMove) j--;
+          if (array[i] === toMove) swap(i, j, array);
+          i++;
+      }
+      return array;
+  }
+  
+  function swap(i, j, array) {
+      let temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
+  }
+  
