0b8bfcc7beabf8cbf8fd91fec6f371f83c86bd92
Claire Moore <mooreclaire95@gmail.com>
2021-03-02 14:27:08 -0500

    Added Caesar Cipher alternative solution

--- AlgoExpert/CaesarCipher.js
+++ AlgoExpert/CaesarCipher.js
@@ -3,11 +3,22 @@ function caesarCipherEncryptor(string, key) {
       key = key % 26
       let strArr = string.split('');
       for (let i = 0; i < strArr.length; i++) {
-          let shiftedIdx = alphabet.indexOf(strArr[i]) + key;
-          if (shiftedIdx >= alphabet.length) {
-              shiftedIdx = shiftedIdx - alphabet.length;
-          }
+          let shiftedIdx = (alphabet.indexOf(strArr[i]) + key) % 26;
               strArr[i] = alphabet[shiftedIdx]
       }
       return strArr.join('');
+  }
+
+  function caesarCipherEncryptor2(string, key) {
+    let newLetters = [];
+      let newKey = key % 26;
+      for (let letter of string) {
+          newLetters.push(getNewLetter(letter, newKey))
+      }
+      return newLetters.join('');
+  }
+  
+  function getNewLetter(letter, key) {
+      const newLetterCode = letter.charCodeAt() + key;
+      return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
   }
