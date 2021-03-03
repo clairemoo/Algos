292104b073f1daca27aaad8c6beb3fe346d5df3a
Claire Moore <mooreclaire95@gmail.com>
2021-03-03 14:54:08 -0500

    Added alternate solution for Monotonic array

--- AlgoExpert/MonotonicArray.js
+++ AlgoExpert/MonotonicArray.js
@@ -16,3 +16,15 @@ function isBreakingDirection(current, prev, direction) {
 	if (direction > 0 && currentDirection < 0) return true;
 	if (direction < 0 && currentDirection > 0) return true;
 }
+
+function isMonotonicCleaner(array) {
+    let nonIncreasing = true;
+      let nonDecreasing = true;
+      
+      for (let i = 1; i < array.length; i++) {
+          if (array[i] < array[i - 1]) nonDecreasing = false;
+          if (array[i] > array[i - 1]) nonIncreasing = false
+      }
+      
+      return nonIncreasing || nonDecreasing
+  }
